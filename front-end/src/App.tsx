820d8aff0418654a7a2de973bfb0d991f75c947d
Natalia Zaitseva <zaitsevn@oregonstate.edu>
2025-10-26 16:03:11 -0700

    Made the middle part of the page!

--- front-end/src/App.tsx
+++ front-end/src/App.tsx
@@ -2,6 +2,7 @@ import oregonLogo from './assets/Oregon_State_text_logo.png'
 import './App.css'
 import Header from './components/Header'
 import Button from './components/Button'
+import mazeImg from './assets/maze.png'
 
 function App() {
 
@@ -19,10 +20,22 @@ function App() {
     <>
       <Header title="Multi-Agent Maze Solver" logoSrc={oregonLogo} actions={header_actions} />
 
-      <div>
-        <p>
-          Middle of the page
-        </p>
+      <div className="maze-section">
+        <div className="maze-text">
+          <h2>
+            A collaborative pathfinding simulation where AI agents explore mazes
+            and communicate to find the fastest route from A to B
+          </h2>
+          <div className="maze-buttons">
+            <Button onClick={() => alert('Build Maze clicked!')}>Build Maze</Button>
+            <Button variant="secondary" onClick={() => alert('Watch Demo clicked!')}>
+              Watch Demo
+            </Button>
+          </div>
+        </div>
+        <div className="maze-image">
+          <img src={mazeImg} alt="Maze Image" />
+        </div>
       </div>
     </>
   )
