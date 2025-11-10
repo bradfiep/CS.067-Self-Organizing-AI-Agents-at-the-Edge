a2542a808d005d52b6d50a090c73d2f171510ae1
Kushp2022 <kushp839@gmail.com>
2025-11-09 16:02:56 -0800

    fix

--- front-end/src/__tests__/setupTests.ts
+++ front-end/src/__tests__/setupTests.ts
@@ -3,16 +3,16 @@ import '@testing-library/jest-dom';
 
 // Mock WebSocket globally to prevent any WebSocket-related errors in tests
 class MockWebSocket {
-  constructor(_url?: string) { 
+  constructor() { 
     // Do nothing - just a stub
   }
   onopen: ((ev: Event) => void) | null = null;
   onclose: ((ev: CloseEvent) => void) | null = null;
   onerror: ((ev: Event) => void) | null = null;
   readyState = 1;
-  send = (..._args: any[]) => {};
+  send = () => {};
   close = () => {};
 }
 
 // Make available globally for tests
-(globalThis as any).WebSocket = MockWebSocket; 
+(globalThis as Record<string, unknown>).WebSocket = MockWebSocket; 
